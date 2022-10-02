bf91a5d6ffdfdf22e69be5cd6dc05d152f1b4315
svvdevell <svvdevel@gmail.com>
2022-10-02 14:44:28 +0300

    added task

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 "use strict"
+
+let albumList = document.getElementById("albums");
+albumList.addEventListener("click", (event) => {
+    let removeBtn = event.target.className === "album_btn";
+    if (removeBtn) {
+        event.target.closest(".album_title").remove();
+    }
+});
+
+fetch("https://jsonplaceholder.typicode.com/albums")
+    .then((response) => response.json())
+    .then((result) => {
+    result.forEach((element) => {
+        let albumTitle = document.createElement("li");
+        let btn = document.createElement("button");
+
+        btn.innerHTML = "Delete";
+        albumTitle.innerHTML = element.title;
+
+        albumList.append(albumTitle);
+        albumTitle.append(btn);
+
+        albumTitle.classList.add("album_title");
+        btn.classList.add("album_btn");
+    });
+})
+    .catch((error) => console.error(error));
